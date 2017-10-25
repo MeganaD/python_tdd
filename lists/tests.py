855489a0f3079665e86fddc2e7d4763290094975
meganad <meganad@outlook.kr>
2017-10-25 11:36:22 +0900

    ch05 62

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,16 @@
+import re
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
-from django.template.loader import render_to_string
+
 
 class HomePageTest(TestCase):
-
+    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
+    
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -20,7 +23,12 @@ class HomePageTest(TestCase):
         # self.assertIn(b'<title>To-Do lists</title>', response.content)
         # self.assertTrue(response.content.strip().endswith(b'</html>'))
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        # self.assertEqual(response.content.decode(), expected_html)
+
+        self.assertEqual(
+          re.sub(self.pattern_input_csrf, '', response.content.decode()),
+          re.sub(self.pattern_input_csrf, '', expected_html)
+      )
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -30,3 +38,4 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertIn('신규 작업 아이템', response.content.decode())
+        
